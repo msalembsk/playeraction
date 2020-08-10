5f543143d300805bedd9a8381a71609602bef121
Med Salem Boussoukaya <salemboussoukaya@gmail.com>
2020-08-10 07:07:50 +0100

    new instat feed code

--- R/instat_events.R
+++ R/instat_events.R
@@ -22,12 +22,6 @@
   away_team_id <- game_info$awayTeamId
   away_team_name <- game_info$awayTeamName
 
-
-
-  action_types <- instat_cfg$action_types
-
-  ## get only useful events
-  events <- events[which(events$generic_action_type_name %in% action_types), ]
   ## number of events row per game
   nrows <- nrow(events)
   ## fill missing bodypart with foot
@@ -36,11 +30,8 @@
   .parse_single_event <- function(idx_row) {
     ## get event by id
     event_ <- events[idx_row, ]
-    spadl_action_name <- .get_action_family(event_)
-
 
     period_id <- event_$half
-    time_in_seconds <- event_$second
 
     player_id <- event_$player_id
     player_name <- event_$player_name
@@ -48,197 +39,279 @@
     team_id <- event_$team_id
     team_name <- event_$team_name
 
+    c(second, minute, time_in_seconds) %<-%
+      .time_in_seconds_minutes(event_$second, event_$half)
+
+    spadl_action_name <- .get_spadl_action_name(event_)
 
     tibble(game_id = game_id,
            home_team_id = home_team_id,
            home_team_name = home_team_name,
            away_team_id = away_team_id,
            away_team_name = away_team_name,
+           period_id = period_id,
+           second = second,
+           minute = minute,
+           time_in_seconds = time_in_seconds,
            player_id = player_id,
            player_name = player_name,
            team_id = team_id,
            team_name = team_name,
-           period_id = period_id,
-           time_in_seconds = time_in_seconds,
            action_name = spadl_action_name)
 
 
   }
-
   ## get all events from a given game_id
-  res <- do.call(rbind, lapply(seq_len(nrows), .parse_single_event))
+  res <- do.call(rbind, lapply(seq_len(nrows), .parse_single_event)) %>%
+    filter(.data$action_name != "non_action")
 
   res
 }
 
+## second/ minute/ time in seconds function
+.time_in_seconds_minutes <- function(time_in_seconds, half_period) {
+  if (half_period == 1) {
+    ## seconds
+    seconds <- time_in_seconds %% 60
+    ## minutes
+    minutes <- as.integer(time_in_seconds / 60)
+  } else {
+    ## seconds
+    seconds <- time_in_seconds %% 60
+    ## minutes second half after 45 minutes
+    minutes <- as.integer(time_in_seconds / 60) + 45
 
-## generic function to get the appropriate action_name
-## TODO : try to replace if else statement to switch case
-.get_action_family <- function(event_,
-                               instat_cfg = .settings$instat_config) {
-  action_type <- event_$generic_action_type_name
+    ## time in seconds after second half adding 45 * 60
+    time_in_seconds <-  time_in_seconds + 2700
+  }
+
+
+  c(seconds, minutes, time_in_seconds)
+}
+
+## function to call the specific fn action
+## TODO : avoid if else statement and replace it with switch case
+.get_spadl_action_name <- function(event_,
+                                   instat_cfg = .settings$instat_config) {
+spadl_action_name <- "non_action"
+ if (event_$generic_action_type_name %in% instat_cfg$shots)
+   spadl_action_name <- .shot_action(event_)
+ else if (event_$action_name %in% instat_cfg$bad_touch)
+   spadl_action_name <- .bad_touch_action(event_)
+ else if (event_$action_name %in% instat_cfg$dribble)
+   spadl_action_name <- .dribble_action(event_)
+ else if (event_$action_subtype_name %in% instat_cfg$take_on)
+   spadl_action_name <- .take_on_action(event_)
+ else if (event_$standart_name %in% instat_cfg$throw_in)
+   spadl_action_name <- .throw_in_action(event_)
+ else if (event_$standart_name %in% instat_cfg$clearance)
+   spadl_action_name <- .clearance_action(event_)
+ else if (event_$action_name %in% instat_cfg$pick_up &
+          event_$position_id %in% instat_cfg$gk_pos_id)
+   spadl_action_name <- .keeper_pick_up_action(event_)
+ else if (event_$action_name %in% instat_cfg$keeper_claim)
+   spadl_action_name <- .keeper_claim_action(event_)
+  else if (event_$action_name %in% instat_cfg$keeper_save)
+   spadl_action_name <- .keeper_save_action(event_)
+  else if (event_$action_subtype_name %in% instat_cfg$tackle)
+    spadl_action_name <- .tackle_action(event_)
+  else if (event_$generic_action_type_name %in% instat_cfg$interception)
+    spadl_action_name <- .interception_action(event_)
+  else if (event_$action_name %in% instat_cfg$foul)
+    spadl_action_name <- .foul_action(event_)
+  else if (event_$generic_action_type_name %in% instat_cfg$cross)
+    spadl_action_name <- .cross_action(event_)
+  else if (event_$standart_name %in% instat_cfg$freekick)
+    spadl_action_name <- .freekick_action(event_)
+  else if (event_$standart_name %in% instat_cfg$corner)
+    spadl_action_name <- .corner_action(event_)
+  else if (event_$standart_name %in% instat_cfg$pass_standart &
+           event_$generic_action_type_name %in% instat_cfg$pass)
+    spadl_action_name <- .pass_action(event_)
+
+ spadl_action_name
+}
+
+## pass
+.pass_action <- function(event_, action_types =
+                           .settings$instat_config$action_types) {
+  action_standart_id <- event_$standart_id
+  spadl_action_name <- action_types[
+    which(action_types$standart_id == action_standart_id), ]$spadl_name
+  spadl_action_name
+}
+
+## corner
+.corner_action <- function(event_, action_types =
+                             .settings$instat_config$action_types) {
+  action_standart_id <- event_$standart_id
   action_subtype_name <- event_$action_subtype_name
-  player_position <- event_$position_name
+  if (action_subtype_name == "set piece") {
+    spadl_action_name <- action_types[
+      which(action_types$standart_id == action_standart_id), ]$spadl_name
+    return(spadl_action_name)
+  }
+  else
+    return("corner_short")
+}
 
-  action_name <- "non_action"
-  if (action_type == "Pass"  & action_subtype_name %in%
-      instat_cfg$action_pass)
-    action_name <- .pass_action_family(event_)
-  else if (action_type == "Shot"  & action_subtype_name %in%
-           instat_cfg$action_shot)
-    action_name <- .shot_action_family(event_)
-  else if (action_type == "Clearance" & action_subtype_name %in%
-           instat_cfg$action_clearance)
-    action_name <- .clearance_action_family(event_)
-  else if (action_type == "Discipline"  & action_subtype_name %in%
-           instat_cfg$action_discipline)
-    action_name <- .discipline_action_family(event_)
-  else if (action_type == "Challenges" & action_subtype_name %in%
-          instat_cfg$action_challenges)
-    action_name <- .challenge_action_family(event_)
-  else if (action_type == "Cross" & action_subtype_name %in%
-           instat_cfg$action_cross)
-    action_name <- .cross_action_family(event_)
-  else if (action_type == "Interception" & action_subtype_name %in%
-           instat_cfg$action_interception)
-    action_name <- .interception_action_family(event_)
-  else if (action_type == "Bad Ball Control" & action_subtype_name %in%
-           instat_cfg$action_bad_control)
-    action_name <- .bad_control_action_family(event_)
-  else if (player_position == "Goalkeeper" & action_subtype_name %in%
-           instat_cfg$action_gk)
-    action_name <- .gk_action_family(event_)
+## freekick
+.freekick_action <- function(event_, action_types =
+                                .settings$instat_config$action_types) {
+  action_standart_id <- event_$standart_id
+  action_subtype_name <- event_$action_subtype_name
+  direct_freekick_id <- 4L
+  indirect_freekick_id <- 3L
+  if (action_standart_id == direct_freekick_id) {
+    if (action_subtype_name == "set piece") {
+      spadl_action_name <- action_types[
+        which(action_types$standart_id == direct_freekick_id), ]$spadl_name
+      return(spadl_action_name)
+    }
+    else
+      return("freekick_short")
+  } else if (action_standart_id == indirect_freekick_id) {
+    if (action_subtype_name == "set piece") {
+      spadl_action_name <- action_types[
+        which(action_types$standart_id == indirect_freekick_id), ]$spadl_name
+      return(spadl_action_name)
+    }
+    else
+      return("freekick_short")
+  }
+}
 
+## cross
+.cross_action <- function(event_, action_types =
+                            .settings$instat_config$action_types) {
+  action_id <- event_$generic_action_type_id
+    spadl_action_name <- action_types[
+      which(action_types$generic_action_type_id == action_id), ]$spadl_name
+  spadl_action_name
+}
 
+.foul_action <- function(event_, action_types =
+                                  .settings$instat_config$action_types) {
+  action_id <- event_$action_id
+  spadl_action_name <- action_types[
+    which(action_types$action_id == action_id), ]$spadl_name
+  spadl_action_name
+}
 
-  action_name
+## cross
+.interception_action <- function(event_, action_types =
+                                  .settings$instat_config$action_types) {
+  action_id <- event_$generic_action_type_id
+  spadl_action_name <- action_types[
+    which(action_types$generic_action_type_id == action_id), ]$spadl_name
+  spadl_action_name
 }
 
 
-## pass action checker
-.pass_action_family <- function(pass_event_,
-                                 instat_cfg = .settings$instat_config) {
-  action_name <- NULL
-  ## check standart action for the right real action
-  standart_action <- pass_event_$standart_name
-
-  throw_in <- standart_action %in% instat_cfg$throw_in
-  ## FIXME : check freekick short or crossed
-  freekick <- standart_action %in% instat_cfg$freekick
-  ## short corner else check cross action + corner attack function
-  ## not sure
-  corner <- standart_action %in% instat_cfg$corner
-  action_name <-
-    dplyr::case_when(throw_in ~ "throw_in",
-                     freekick ~ "freekick",
-                     corner ~ "corner",
-                     TRUE ~ "pass")
-  action_name
+## tackle
+.tackle_action <- function(event_, action_types =
+                             .settings$instat_config$action_types) {
+  action_name <- event_$action_subtype_name
+  spadl_action_name <- action_types[
+    which(action_types$action_subtype_name == action_name), ]$spadl_name
+  spadl_action_name
 }
 
-## interception action
-.interception_action_family <- function(interception_event_,
-                                        instat_cfg = .settings$instat_config) {
-  interception_action  <- interception_event_$action_subtype_name %in%
-    instat_cfg$action_interception
-  if (interception_action)
-    return("interception")
+
+## GK save
+.keeper_save_action <- function(event_, action_types =
+                                   .settings$instat_config$action_types) {
+  action_id <- event_$action_id
+  spadl_action_name <- action_types[
+    which(action_types$action_id == action_id), ]$spadl_name
+  spadl_action_name
 }
 
+## GK claim
+.keeper_claim_action <- function(event_, action_types =
+                                  .settings$instat_config$action_types) {
+  action_id <- event_$action_id
+  spadl_action_name <- action_types[
+    which(action_types$action_id == action_id), ]$spadl_name
+  spadl_action_name
+}
+
+## GK pickup
+.keeper_pick_up_action <- function(event_, action_types =
+                                     .settings$instat_config$action_types) {
+
+action_id <- event_$action_id
+spadl_action_name <- action_types[
+  which(action_types$action_id == action_id), ]$spadl_name
+spadl_action_name
+}
+
+
+## throw in
+.throw_in_action <- function(event_, action_types =
+                               .settings$instat_config$action_types) {
+  standart_id <- event_$standart_id
+
+  spadl_action_name <- action_types[
+    which(action_types$standart_id == standart_id), ]$spadl_name
+  spadl_action_name
+}
+
+## take on
+.take_on_action <- function(event_, action_types =
+                              .settings$instat_config$action_types) {
+  action_name <- event_$action_subtype_name
+
+  spadl_action_name <- action_types[
+    which(action_types$action_subtype_name == action_name), ]$spadl_name
+  spadl_action_name
+}
+
+## clearance
+.clearance_action <- function(event_, action_types =
+                              .settings$instat_config$action_types) {
+  action_id <- event_$action_id
+
+  spadl_action_name <- action_types[
+    which(action_types$action_id == action_id), ]$spadl_name
+  spadl_action_name
+}
+
+## dribble
+.dribble_action <- function(event_, action_types =
+                              .settings$instat_config$action_types) {
+  action_id <- event_$action_id
+
+  spadl_action_name <- action_types[
+    which(action_types$action_id == action_id), ]$spadl_name
+  spadl_action_name
+}
 
 ## bad ball control
-.bad_control_action_family <- function(bad_control_event_,
-                                       instat_cfg = .settings$instat_config) {
-  ball_touch_action <- bad_control_event_$action_subtype_name %in%
-    instat_cfg$action_bad_control
+.bad_touch_action <- function(event_, action_types =
+                                .settings$instat_config$action_types) {
+  action_id <- event_$action_id
 
-  if (ball_touch_action)
-    return("ball_touch")
+  spadl_action_name <- action_types[
+    which(action_types$action_id == action_id), ]$spadl_name
+  spadl_action_name
 }
 
-## cross action
-.cross_action_family <- function(cross_event_,
-                                 instat_cfg = .settings$instat_config) {
+## shot/ penalty shot/ freekick shot
+.shot_action <- function(event_,
+                         shot_types = .settings$instat_config$shot_types) {
 
-  cross_action <- cross_event_$action_subtype_name %in% instat_cfg$cross
-  if (cross_action)
-    return("cross")
-}
+  action_standart_id <- event_$standart_id
+  action_attack_type_id <- event_$attack_type_id
+  if (is.element(action_attack_type_id, shot_types$attack_type_id)) {
+    spadl_action <- shot_types[
+      which(shot_types$attack_type_id == action_attack_type_id), ]$spadl_name
+    return(spadl_action)
+  } else if (is.element(action_standart_id, shot_types$standart_id)) {
+    spadl_action <- shot_types[
+      which(shot_types$standart_id == action_standart_id), ]$spadl_name
+    return(spadl_action)
+  } else
+    return("shot")
 
-
-## shot action checker
-.shot_action_family <- function(shot_event_,
-                                instat_cfg = .settings$instat_config) {
-  action_name <- NULL
-  ## check standart action for freekick shot
-  shot_freekick <- shot_event_$standart_name %in% instat_cfg$shot_freekick
-
-  if (shot_freekick)
-    action_name <- "shot_freekick"
-  else
-    action_name <- "shot"
-
-  action_name
-}
-
-## discipline action
-.discipline_action_family <- function(discipline_event_,
-                                       instat_cfg = .settings$instat_config) {
-  action_name <- NULL
-  foul_action <- discipline_event_$action_subtype_name %in% instat_cfg$foul
-  if (foul_action)
-    action_name <- "foul"
-
-  action_name
-}
-
-## challenges actions
-.challenge_action_family <- function(challenge_event_,
-                                     instat_cfg = .settings$instat_config) {
-  action_name <- NULL
-  action_type <- challenge_event_$action_subtype_name
-
-  take_on <- action_type %in% instat_cfg$take_on
-  dribble <- action_type %in% instat_cfg$dribble
-  tackle <- action_type %in% instat_cfg$tackle
-  action_name <-
-    dplyr::case_when(take_on ~ "take_on",
-                     dribble ~ "dribble",
-                     tackle ~ "tackle",
-                     TRUE ~ "non_action")
-  action_name
-
-}
-
-## clearance action
-.clearance_action_family <- function(clearance_event_,
-                                      instat_cfg = .settings$instat_config) {
-  action_name <- NULL
-  clearance_action <- clearance_event_$action_subtype_name %in%
-    instat_cfg$action_clearance
-
-  if (clearance_action)
-    action_name <- "clearance"
-
-  action_name
-}
-
-.gk_action_family <- function(gk_event_,
-                              instat_cfg = .settings$instat_config) {
-  action_name <- NULL
-
-  action_type <- gk_event_$action_subtype_name
-
-  gk_interception <- action_type %in% instat_cfg$gk_interception
-  keeper_save <- action_type %in% instat_cfg$keeper_save
-  keeper_pick_up <- action_type %in% instat_cfg$keper_pick_up
-
-  if (keeper_save)
-    action_name <- "keeper_save"
-  else if (gk_interception)
-    action_name <- "interception"
-  else if (keeper_pick_up)
-    action_name <- "keeper_pick_up"
-
-  action_name
 }
--- inst/examples/ex_spadl.R
+++ inst/examples/ex_spadl.R
@@ -8,7 +8,7 @@ library(magrittr)
 
 devtools::load_all()
 playeraction::set_db_all(database_type = "local", data_provider = "inStat")
-inStat_events <- playeraction::.instat_events_from_game(1171033)
+inStat_events <- playeraction::.instat_events_from_game(1485159)
 game_id <- 44108
 events <- playeraction::.opta_events_from_game(game_id)
 spadl_events <- playeraction::convert_events_to_spadl.opta_events(events)
