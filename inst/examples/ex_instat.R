5670e5fad219214438f46cd09d4e2039f2b7f9be
Med Salem Boussoukaya <salemboussoukaya@gmail.com>
2020-09-13 17:07:35 +0100

    minor changes

--- inst/examples/ex_instat.R
+++ inst/examples/ex_instat.R
@@ -7,12 +7,14 @@ library(lubridate)
 library(magrittr)
 
 ## Leicester vs Man utd
-game_id <- 1485159L
+## game_id <- 1171071L
+
 devtools::load_all()
 playeraction::set_db(database_type = "local", data_provider = "inStat")
-spadl_instat <- SpadlInStat$new(game_id, spadl_type = "standard")
+
+game_ids <- .settings$gameEvents_con$distinct("gameId")
+spadl_instat <- SpadlInStat$new(game_ids, spadl_type = "atomic")
 flog.info(paste("Spadl representation created !"))
 spadl_instat$get_model_data(add_predictions = TRUE)
 flog.info(paste("training data created !"))
-
 
